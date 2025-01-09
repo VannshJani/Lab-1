d80ad558729c3d80ca92fb7e44e698bbd76776b5
Vannsh <v@Vannshs-MacBook-Pro.local>
2025-01-09 09:29:30 +0530

    Added new python file

--- sample.py
+++ sample.py
@@ -1,43 +1,96 @@
-import pdb
-import src
-import glob
-import importlib.util as ut
-import os
-import cv2
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def is_valid(board, row, col, num):
+    for i in range(9):
+        if board[row][i] == num or board[i][col] == num:
+            return False
+
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+    return True
+
+def find_mrv(board):
+    min_count = 10
+    mrv_position = (-1, -1)
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                count = sum(is_valid(board, row, col, num) for num in range(1, 10))
+                if count < min_count:
+                    min_count = count
+                    mrv_position = (row, col)
+
+    # print("MRV Position:", mrv_position)
+    return mrv_position
+
+all_boards = []
+
+def solve_sudoku(board):
+    all_boards.append([row[:] for row in board])
+    empty = find_mrv(board)
+    if empty == (-1, -1):
+        return True
+
+    row, col = empty
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+            if solve_sudoku(board):
+                return True
+            board[row][col] = 0
+
+    return False
+
+def print_board(board):
+    for row in board:
+        print(" ".join(str(num) if num != 0 else '.' for num in row))
+
+# Example Sudoku board with 0 representing empty cells
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+if solve_sudoku(board):
+    print("Solved Sudoku Board:")
+    print_board(board)
+    print("Number of boards:", len(all_boards))
+else:
+    print("No solution exists.")
 
 
+def animate_sudoku(all_boards):
+    fig, ax = plt.subplots()
 
-### Change path to images here
-path = '/Users/vannshjani/Desktop/CV3/ES666-Assignment3/Images/*'  # Use os.sep, Windows, linux have different path delimiters
-###
+    def update(frame):
+        ax.clear()
+        ax.set_xticks([i + 0.5 for i in range(9)], minor=True)
+        ax.set_yticks([i + 0.5 for i in range(9)], minor=True)
+        ax.grid(which="minor", color="black", linestyle='-', linewidth=2)
+        ax.imshow([[5]*9]*9, cmap="Blues", vmin=0, vmax=9)
+        
+        for i in range(9):
+            for j in range(9):
+                num = all_boards[frame][i][j]
+                if num != 0:
+                    ax.text(j, i, str(num), ha="center", va="center", color="black")
+        
+        ax.set_xticks([])
+        ax.set_yticks([])
 
-all_submissions = glob.glob('./src/*')
-os.makedirs('./results/', exist_ok=True)
-for idx,algo in enumerate(all_submissions):
-    print('****************\tRunning Awesome Stitcher developed by: {}  | {} of {}\t********************'.format(algo.split(os.sep)[-1],idx,len(all_submissions)))
-    try:
-        module_name = '{}_{}'.format(algo.split(os.sep)[-1],'stitcher')
-        filepath = '{}{}stitcher.py'.format( algo,os.sep,'stitcher.py')
-        spec = ut.spec_from_file_location(module_name, filepath)
-        module = ut.module_from_spec(spec)
-        spec.loader.exec_module(module)
-        PanaromaStitcher = getattr(module, 'PanaromaStitcher')
-        inst = PanaromaStitcher()
+    ani = animation.FuncAnimation(fig, update, frames=len(all_boards), interval=500, repeat=False)
+    ani.save("sudoku_animation.gif", writer="imagemagick")
+    plt.show()
 
-        ###
-        scale_factors = [0.25,1,1,0.5,0.5,1]
-        scale_idx = 0 
-        for impaths in sorted(glob.glob(path)):
-            print('\t\t Processing... {}'.format(impaths))
-            stitched_image, homography_matrix_list = inst.make_panaroma_for_images_in(path=impaths,scale_factor_x=scale_factors[scale_idx],scale_factor_y=scale_factors[scale_idx])
-            scale_idx+=1
-            outfile =  './results/{}/{}.png'.format(impaths.split(os.sep)[-1],spec.name)
-            os.makedirs(os.path.dirname(outfile),exist_ok=True)
-            cv2.imwrite(outfile,stitched_image)
-            print(homography_matrix_list)
-            print('Panaroma saved ... @ ./results/{}.png'.format(spec.name))
-            print('\n\n')
-
-    except Exception as e:
-        print('Oh No! My implementation encountered this issue\n\t{}'.format(e))
-        print('\n\n')
+animate_sudoku(all_boards)
